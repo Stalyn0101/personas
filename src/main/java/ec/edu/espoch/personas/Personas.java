a6be6ec9c33dfe0aa93806ff8fcc85b316df22be
Stalyn0101 <yixon11@gmail.com>
2024-11-05 15:41:52 -0500

    hola

---
+++ src/main/java/ec/edu/espoch/personas/Personas.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.personas;
+
+/**
+ *
+ * @author USER
+ */
+public class Personas {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
